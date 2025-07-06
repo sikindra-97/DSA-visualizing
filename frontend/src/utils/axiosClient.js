ae7437d81c8a7b93359c7e4a025ea30ac09823cb
sikindra <sikindrak884@gmail.com>
2025-07-06 18:24:21 +0530

    Point axiosClient to Render backend

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:4000/api', // Make sure this matches your backend
+  baseURL: 'https://dsa-visualizing-3.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
